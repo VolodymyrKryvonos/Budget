ffe4fd63dde5e1ced4915993e8e94ef1ad6c8c6a
VovaKryvonos <kruvonosvova@gmail.com>
2019-02-12 22:08:00 +0200

    z

--- app/src/main/java/com/example/vova/budjet/MainActivity.java
+++ app/src/main/java/com/example/vova/budjet/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,InfoActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
